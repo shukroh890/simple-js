28ab094724274332f59a292a74e5e792f1dc8010
shukroh890 <105241468+shukroh890@users.noreply.github.com>
2022-08-07 05:09:20 +0100

    Add files via upload

---
+++ life.js
@@ -0,0 +1,19 @@
+const num1 = parseFloat(prompt("Enter nr1: "));
+const num2 = parseFloat(prompt("Enter nr2: "));
+
+const operator = prompt("Enter operator(+,-,/,*)")
+let result = 0;
+if(isNaN(num1) || isNaN(num2)){
+    alert("wrong input! Refreash the page and provide details")
+}else{
+    if(operator == "+"){
+        result = num1 + num2;
+    }else if (operator == "-"){
+        result = num1 - num2;
+    }else if (operator == "/"){
+        result = num1 / num2;
+    }else if (operator == "*"){
+        result = num1 * num2;
+    }
+    alert(num1+operator+num2+"=" + result);
+}
